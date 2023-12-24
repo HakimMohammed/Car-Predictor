687a0693b80e938774f0dcfb3f889a5f163c3aa2
Hakim Mohamed <mohmadhakim1234@gmail.com>
2023-12-24 15:34:55 +0100

    Loading and Welcome Page

--- app/src/main/java/com/example/carpredictor/MainActivity.java
+++ app/src/main/java/com/example/carpredictor/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.carpredictor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static int TIME_OUT = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, WelcomePage.class);
+                startActivity(intent);
+                finish();
+            }
+        }, TIME_OUT);
+
     }
 }
---
+++ app/src/main/java/com/example/carpredictor/WelcomePage.java
@@ -0,0 +1,14 @@
+package com.example.carpredictor;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_page);
+    }
+}
